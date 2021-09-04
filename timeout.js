c61c1086affc72b9d1738274103fd32eb35e3685
Allahr Golam <callmesajib.me@gmail.com>
2021-09-04 21:44:07 +0600

    first commit

---
+++ timeout.js
@@ -0,0 +1,19 @@
+function doSomething() {
+    console.log('I am coading JavaScript')
+}
+// synchronously
+// const miliSecondToSecond = miliSecond =>{
+//     const result = miliSecond * 1000;
+//     return result;
+// }
+console.log('First: ami sovar age');
+console.log('Second: Iam the second Person');
+// setTimeout(doSomething,6000 );
+setTimeout(function(){
+    console.log('i am using vs code')
+},5000);
+setTimeout( () => {
+  console.log('Exploring MDN articles');
+}, 4000)
+console.log('Third: ami 3 number baccha');
+console.log('Fourth: ami ek hali purno korci')
